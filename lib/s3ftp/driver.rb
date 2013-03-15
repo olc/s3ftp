f3c702853f979eed40f13016eb0ebec9c98d6f34
Deepak Vig <deepakvig@gmail.com>
2013-03-15 11:24:06 +0530

    Initial commit

---
+++ lib/s3ftp/driver.rb
@@ -0,0 +1,232 @@
+# coding: utf-8
+
+module S3FTP
+  class Driver
+
+    USER  = 0
+    PASS  = 1
+    ADMIN = 2
+
+    def initialize(config, passwd)
+      @config = config
+      @users  = {}
+      CSV.parse(passwd).map { |row|
+        @users[row[USER]] = {
+          :pass  => passwordfile_uses_bcrypt ? BCrypt::Password.new(row[PASS]) : row[PASS], 
+          :admin => row[ADMIN].to_s.upcase == "Y"
+        }
+      }
+    end
+
+    def change_dir(path, &block)
+      prefix = scoped_path(path)
+
+      item = Happening::S3::Bucket.new(aws_bucket, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret, :prefix => prefix, :delimiter => "/")
+      item.get do |response|
+        yield contains_directory?(response.response, prefix)
+      end
+    end
+
+    def dir_contents(path, &block)
+      puts path
+      puts "Interesting"
+      prefix = scoped_path_with_trailing_slash(path)
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield response.response_header["CONTENT_LENGTH"].to_i }
+
+      item = Happening::S3::Bucket.new(aws_bucket, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret, :prefix => prefix, :delimiter => "/")
+      item.get do |response|
+        puts parse_bucket_list(response.response)
+        yield parse_bucket_list(response.response)
+      end
+    end
+
+    def authenticate(user, pass, &block)
+      if @users[user] && @users[user][:pass] == pass
+        @user = user
+        yield true
+      else
+        yield false
+      end
+    end
+
+    def bytes(path, &block)
+      key = scoped_path(path)
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield response.response_header["CONTENT_LENGTH"].to_i }
+
+      item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.head(:retry_count => 0, :on_success => on_success, :on_error => on_error)
+    end
+
+    def get_file(path, &block)
+      key = scoped_path(path)
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield response.response }
+
+      item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.get(:retry_count => 1, :on_success => on_success, :on_error => on_error)
+    end
+
+    def put_file(path, tmp_path, &block)
+      key = scoped_path(path)
+
+      bytes      = File.size(tmp_path)
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield bytes  }
+
+      item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.put(File.binread(tmp_path), :retry_count => 0, :on_success => on_success, :on_error => on_error)
+    end
+
+    def delete_file(path, &block)
+      key = scoped_path(path)
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield true  }
+
+      item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.delete(:retry_count => 1, :on_success => on_success, :on_error => on_error)
+    end
+
+    def delete_dir(path, &block)
+      prefix = scoped_path(path)
+
+      on_error   = Proc.new {|response| yield false }
+
+      item = Happening::S3::Bucket.new(aws_bucket, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret, :prefix => prefix)
+      item.get(:on_error => on_error) do |response|
+        keys = bucket_list_to_full_keys(response.response)
+        delete_object = Proc.new { |key, iter|
+          item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+          item.delete(:retry_count => 1, :on_error => on_error) do |response|
+            iter.next
+          end
+        }
+        on_complete = Proc.new { yield true }
+
+        EM::Iterator.new(keys, 5).each(delete_object, on_complete)
+      end
+    end
+
+    def rename(from, to, &block)
+      source_key = scoped_path(from)
+      source_obj = aws_bucket + "/" + source_key
+      dest_key   = scoped_path(to)
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield true  }
+
+      item = Happening::S3::Item.new(aws_bucket, dest_key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.put(nil, :retry_count => 1, :on_error => on_error, :headers => {"x-amz-copy-source" => source_obj}) do |response|
+        item = Happening::S3::Item.new(aws_bucket, source_key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+        item.delete(:retry_count => 1, :on_success => on_success, :on_error => on_error)
+      end
+    end
+
+    def make_dir(path, &block)
+      key = scoped_path(path) + "/.dir"
+
+      on_error   = Proc.new {|response| yield false }
+      on_success = Proc.new {|response| yield true  }
+
+      item = Happening::S3::Item.new(aws_bucket, key, :aws_access_key_id => aws_key, :aws_secret_access_key => aws_secret)
+      item.put("", :retry_count => 0, :on_success => on_success, :on_error => on_error)
+    end
+
+    private
+
+    def admin?
+      @users[@user] && @users[@user][:admin]
+    end
+
+    def scoped_path_with_trailing_slash(path)
+      path  = File.join("/", path)
+      path += "/" if path[-1,1] != "/"
+      path == "/" ? nil : path[1..-1]
+    end
+
+    def scoped_path(path)
+      path = "" if path == "/"
+      if admin?
+      	 File.join("/", path)[1,1024]
+      else
+        File.join("/", @user, path)[1,1024]
+      end
+    end
+
+    def aws_bucket
+      @config[:bucket]
+    end
+
+    def aws_key
+      @config[:aws_key]
+    end
+
+    def aws_secret
+      @config[:aws_secret]
+    end
+
+    def passwordfile_uses_bcrypt
+      @config[:remote_passwd_use_bcrypt]
+    end
+    
+    def bucket_list_to_full_keys(xml)
+      doc = Nokogiri::XML(xml)
+      doc.remove_namespaces!
+      doc.xpath('//Contents').map { |node|
+        node.xpath('./Key').first.content
+      }
+    end
+
+    def contains_directory?(xml, path)
+      doc = Nokogiri::XML(xml)
+      doc.remove_namespaces!
+      prefix = doc.xpath('/ListBucketResult/Prefix').first.content
+
+      doc.xpath('//CommonPrefixes').any? { |node|
+        name  = node.xpath('./Prefix').first.content
+
+        name.to_s.start_with?(prefix)
+      }
+    end
+
+    def parse_bucket_list(xml)
+      doc = Nokogiri::XML(xml)
+      doc.remove_namespaces!
+      prefix = doc.xpath('/ListBucketResult/Prefix').first.content
+      files = doc.xpath('//Contents').select { |node|
+        name  = node.xpath('./Key').first.content
+        bytes = node.xpath('./Size').first.content.to_i
+        name != prefix && bytes > 0
+      }.map { |node|
+        name  = node.xpath('./Key').first.content
+        bytes = node.xpath('./Size').first.content
+        file_item(name[prefix.size, 1024], bytes)
+      }
+      dirs = doc.xpath('//CommonPrefixes').select { |node|
+        node.xpath('./Prefix').first.content != prefix + "/"
+      }.map { |node|
+        name  = node.xpath('./Prefix').first.content
+        dir_item(name[prefix.size, 1024].tr("/",""))
+      }
+      default_dirs + dirs + files
+    end
+
+    def default_dirs
+      [dir_item("."), dir_item("..")]
+    end
+
+    def dir_item(name)
+      EM::FTPD::DirectoryItem.new(:name => name, :directory => true, :size => 0)
+    end
+
+    def file_item(name, bytes)
+      EM::FTPD::DirectoryItem.new(:name => name, :directory => false, :size => bytes)
+    end
+
+  end
+end
---
+++ s3ftp.gemspec
@@ -0,0 +1,32 @@
+lib = File.expand_path('../lib/', __FILE__)
+$:.unshift lib unless $:.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name = "s3ftp"
+  spec.version = "0.0.2"
+  spec.summary = "An FTP proxy in front of an Amazon S3 bucket"
+  spec.description = "Run an FTP server that persists all data to an Amazon S3 bucket"
+  #spec.files =  Dir.glob("{bin,lib}/**/**/*") + ["Gemfile", "README.markdown","MIT-LICENSE"]
+  spec.has_rdoc = true
+  spec.extra_rdoc_files = %w{README.markdown MIT-LICENSE }
+  spec.rdoc_options << '--title' << 'S3-FTP Documentation' <<
+                       '--main'  << 'README.markdown' << '-q'
+  spec.authors = ["Deepak Vig"]
+  spec.email   = ["deepakvig@gmail.com"]
+  spec.homepage = "http://github.com/deepakvig/s3ftp"
+  spec.required_ruby_version = ">=1.9.2"
+
+  spec.add_development_dependency("rake")
+  spec.add_development_dependency("rspec", "~>2.6")
+
+  spec.add_dependency('em-ftpd')
+  spec.add_dependency('happening')
+  spec.add_dependency('nokogiri')
+  spec.add_dependency('trollop')
+  spec.add_dependency('bcrypt-ruby')
+  spec.executables = ["s3ftp"]
+  spec.files = `git ls-files`.split("\n")
+  spec.test_files = `git ls-files -- {test,spec,features}/*`.split("\n")
+  spec.executables = ["s3ftp"]
+  spec.require_path = 'lib'
+end
